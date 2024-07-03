0c6cd4601212f87319dd2b403a016e9f8decf4d3
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-03 21:50:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
     //Write your code here
-  
+  let volume;
+  let radius =document.getElementById('radius').value;
+  // radius = Math.abs(radius);
+	console.log(typeof(radius))
+	if(radius>=0){
+		volume = (4/3) * Math.PI * Math.pow(radius, 3);
+	    volume = volume.toFixed(4);
+	}else{
+		volume='NaN';
+	}
+	document.getElementById('volume').value = volume;
+	return false
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
